fb474bcef02fc277b18e9fc62045de7da21b8311
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-10-24 00:05:36 +0900

    add export config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withTwin = require("./withTwin.js")
  * @type {import('next').NextConfig}
  */
 module.exports = withTwin({
+  output: "export",
   reactStrictMode: true,
   experimental: {
     serverActions: true,
