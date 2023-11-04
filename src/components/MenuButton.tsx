6a58cfbbcf78cc3e45f96ad5bc39d7c8e4b95739
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-11-04 11:36:29 +0900

    create home button

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,29 @@
+/** @jsxImportSource @emotion/react */
+
+"use client"
+
+import { css } from "@emotion/react"
+import Link from "next/link"
+import tw from "twin.macro"
+
+const button = css({
+  ...tw`
+        absolute
+        fixed
+        bottom-2 right-2
+        btn
+        z-10
+    `,
+})
+
+function MenuButton() {
+  return (
+    <>
+      <Link css={button} href={"/"}>
+        HOME
+      </Link>
+    </>
+  )
+}
+
+export default MenuButton
