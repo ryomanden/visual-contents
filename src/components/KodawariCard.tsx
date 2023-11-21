b62fcedb9656101d0970d2d41804788918bf03b1
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-11-22 00:35:36 +0900

    Create card

---
+++ src/components/KodawariCard.tsx
@@ -0,0 +1,62 @@
+/** @jsxImportSource @emotion/react */
+
+"use client"
+
+import { css } from "@emotion/react"
+import Image from "next/image"
+import tw from "twin.macro"
+
+interface Props {
+  title: string
+  subtitle?: string
+  image: string[]
+  description?: string
+}
+const contentWrap = css({
+  ...tw`
+        w-full h-auto
+        flex flex-col
+        justify-center items-center
+        py-20
+        border-b-2 border-gray-100
+    `,
+})
+
+const textWrap = css({
+  ...tw`
+        flex
+        py-5
+        w-full max-w-3xl flex-col
+        justify-center
+        items-center
+    `,
+})
+
+function KodawariCard({ title, subtitle, image, description }: Props) {
+  return (
+    <>
+      <div css={contentWrap}>
+        <h3 tw={"badge badge-neutral badge-lg"}>{title}</h3>
+        <h2 tw={"block pt-2 text-3xl font-bold"}>{subtitle}</h2>
+        <p css={textWrap}>{description}</p>
+        <div
+          tw={"w-full flex-wrap h-auto grid grid-flow-col gap-2 max-w-4xl h-96"}
+        >
+          {image.map(img => (
+            // eslint-disable-next-line react/jsx-key
+            <div tw={"relative w-full h-full aspect-square"}>
+              <Image
+                tw={"object-cover rounded-lg shadow"}
+                src={img}
+                alt="image"
+                fill
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default KodawariCard
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as TopMenu } from "./TopMenu"
 export { default as ButtonOutline } from "./ButtonOutline"
 export { default as MenuButton } from "./MenuButton"
+export { default as KodawariCard } from "./KodawariCard"
