a7db224b9b1a5577d69a7b8c1c0eb14b02c27b77
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2023-11-24 00:42:08 +0900

    add logo data and little fix program

--- src/app/italian/page.tsx
+++ src/app/italian/page.tsx
@@ -114,15 +114,7 @@ function App() {
   return (
     <div tw={"flex flex-col items-center"}>
       <header css={headerStyle}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          tw={"pr-3"}
-          viewBox="0 0 576 512"
-        >
-          <path d="M432 240c53 0 96-43 96-96s-43-96-96-96c-35.5 0-66.6 19.3-83.2 48H296.2C316 40.1 369.3 0 432 0c79.5 0 144 64.5 144 144s-64.5 144-144 144c-27.7 0-53.5-7.8-75.5-21.3l35.4-35.4c12.2 5.6 25.8 8.7 40.1 8.7zM1.8 142.8C5.5 133.8 14.3 128 24 128H392c9.7 0 18.5 5.8 22.2 14.8s1.7 19.3-5.2 26.2l-177 177V464h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H208 120c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V345.9L7 169c-6.9-6.9-8.9-17.2-5.2-26.2z" />
-        </svg>
-        <h1 css={titleStyle}>Italian CIT</h1>
+        <img tw={"w-40"} src="/italian/Logo_italianCIT.svg" />
       </header>
       <div tw={"hero min-h-screen"}>
         <div tw={"h-full -z-1"}>
@@ -179,15 +171,7 @@ function App() {
       </div>
       <footer tw={"footer p-10 bg-neutral text-neutral-content"}>
         <aside tw={"flex flex-row h-max"}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            tw={"fill-current text-3xl"}
-            viewBox="0 0 512 512"
-          >
-            <path d="M432 240c53 0 96-43 96-96s-43-96-96-96c-35.5 0-66.6 19.3-83.2 48H296.2C316 40.1 369.3 0 432 0c79.5 0 144 64.5 144 144s-64.5 144-144 144c-27.7 0-53.5-7.8-75.5-21.3l35.4-35.4c12.2 5.6 25.8 8.7 40.1 8.7zM1.8 142.8C5.5 133.8 14.3 128 24 128H392c9.7 0 18.5 5.8 22.2 14.8s1.7 19.3-5.2 26.2l-177 177V464h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H208 120c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V345.9L7 169c-6.9-6.9-8.9-17.2-5.2-26.2z" />
-          </svg>
-          <p tw={"font-bold text-3xl"}>Italian CIT</p>
+          <img tw={"w-44 h-auto"} src="/italian/Logo_italianCIT_white.svg" />
         </aside>
         <nav>
           <header tw={"footer-title"}>Address</header>
